10982fc9e417c917d9aa34ecea5f1b5521f6b459
Dmitriy <kerokero@yandex.ru>
2025-12-30 09:29:01 +0300

    Fix linter errors in imports

--- hexlet_django_blog/articles/urls.py
+++ hexlet_django_blog/articles/urls.py
@@ -1,16 +1,19 @@
 from django.urls import path
 
-from hexlet_django_blog.articles import views
-from hexlet_django_blog.articles.views import IndexView, ArticleView, ArticleFormCreateView
+from hexlet_django_blog.articles.views import (
+    IndexView,
+    ArticleView,
+    ArticleFormCreateView,
+)
 
 urlpatterns = [
     path('', IndexView.as_view(), name='articles'),
     path('<int:id>/', ArticleView.as_view()),
     path('create/', ArticleFormCreateView.as_view(), name='articles_create'),
     # path('', views.ArticleIndexView.as_view(), name='articles'),
-    #path(
-    #    '<str:tags>/<int:article_id>/',
-    #    views.articles_detail,
-    #    name='article_detail'
-    #),
+    # path(
+    #     '<str:tags>/<int:article_id>/',
+    #     views.articles_detail,
+    #     name='article_detail'
+    # ),
 ]
